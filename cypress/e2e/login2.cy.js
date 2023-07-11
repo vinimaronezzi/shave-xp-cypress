64bfd2edcd21771a65da51c05fa3f9a0b660f97e
Vinicius maronezzi <vinicius.sabel@gmail.com>
2023-07-10 21:43:11 -0300

    sei la bicho

--- cypress/e2e/login2.cy.js
+++ cypress/e2e/login2.cy.js
@@ -8,7 +8,7 @@ describe('login', () => {
 
   context('quando submeto o formulário', () => {
 
-    it.only('deve logar com sucesso', () => {
+    it('deve logar com sucesso', () => {
       
       const user = data.loginsucess
       cy.createUser(user)
